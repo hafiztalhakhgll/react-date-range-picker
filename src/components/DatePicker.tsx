d7ca4e53883da4168b952c2ca574f9404e78ebf4
Talha Khalid <talha.khalid@greenlightlabs.tech>
2023-01-03 14:35:40 +0500

    Working on Date PIcker Month Section

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -5,9 +5,9 @@ const DatePicker = () => {
   return (
     <div>
       <div className='date-picker-layer'>
-        {/* {monthTableData.map(({ year, month }) => (
+        {monthTableData.map(({ year, month }) => (
           <MonthTable key={`${year}${month}`} year={year} month={month} />
-        ))} */}
+        ))}
         <span className='prevbtn-prevbtn'>
           <Prev />
         </span>
